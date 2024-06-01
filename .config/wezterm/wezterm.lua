6b5f3a27cb30910841bce5b07483005e13c760b7
mkhoatd <mkhoatd@mkhoatds-MacBook-Pro.local>
2024-06-01 16:53:31 +0700

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,14 @@ local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+local opacity = 0.75
+local transparent_bg = "rgba(22, 24, 26, " .. opacity .. ")"
 
 config.enable_scroll_bar = true
-config.window_background_opacity = 0.8
+config.window_background_opacity = opacity
+config.win32_system_backdrop = "Acrylic"
 config.font = wezterm.font("JetbrainsMono Nerd Font")
+config.colors = require("cyberdream")
 
 config.ssh_domains = {
 	{
@@ -16,6 +20,28 @@ config.ssh_domains = {
 		remote_address = "dg1.mkhoatd.lol",
 		-- The username to use on the remote host
 		username = "root",
+	},
+}
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.colors.tab_bar = {
+	background = transparent_bg,
+	active_tab = {
+		bg_color = transparent_bg,
+		fg_color = "#ffffff",
+	},
+	inactive_tab = {
+		bg_color = transparent_bg,
+		fg_color = "#7b8496",
+	},
+	inactive_tab_hover = {
+		bg_color = transparent_bg,
+		fg_color = "#ffffff",
+	},
+	new_tab = {
+		bg_color = transparent_bg,
+		fg_color = "#ffffff",
 	},
 }
 
